9995cbdeb55eb2e7ce09112a5cd7d4e50fb13211
Ignacio Lladó Vidal <nachollv@uoc.edu>
2022-11-20 11:42:47 +0100

    He añadido un button offcanvas en el menú principal

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,6 +11,9 @@ import * as bootstrap from 'bootstrap';
  * Write any other JavaScript below
  */
 
+ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl)) 
+
 +( function() {
   const university = "UOC";
   console.log(`Hello, ${university}!`);
